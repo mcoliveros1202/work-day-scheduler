0ace8e5522e5a7682c6fa9628cb61527ae364750
moliveros <mcoliveros1202@gmail.com>
2022-05-30 16:35:46 -0500

    created time blocks and added save buttons

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+var events = {}
+// display current date: moment().format('LL');
+var date = moment().format("LL");
+
+
+// create an event
+var createEvent = function(eventText, eventTime, eventList) {
+    var eventLi = $("<li>").addClass("list-group-item");
+    var eventSpan = $("<span>").addClass("").text(eventTime);
+    var eventP = $("<p>").addClass("m-1").text(eventText);
+
+    // append span and p element to parent li
+  eventLi.append(eventSpan, eventP);
+
+  // check due date
+  auditEvent(eventLi);
+
+  // append to ul list on the page
+  $("#list-" + eventList).append(eventLi);
+}
+
+// load events
+
+// save events
+var saveEvents = function () {
+    localStorage.setItem("events", JSON.stringify(events));
+    console.log("event saved")
+  };
+
+  // event text was clicked
+$(".list-group").on("click", "p", function () {
+    // get current text of p element
+    var text = $(this)
+      .text()
+      .trim();
+  
+    // replace p element with a new textarea
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
+
+      // update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveEvents();
+
+  // recreate p element
+  var eventP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with new content
+  $(this).replaceWith(eventP);
+});
