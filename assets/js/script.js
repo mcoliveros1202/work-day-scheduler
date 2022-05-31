b2e465f2b5b955c105f8ab042ce8f1b7a039c282
moliveros <mcoliveros1202@gmail.com>
2022-05-31 12:37:54 -0500

    started color coding time-audit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,56 @@
 // display current date
 $("#currentDay").text(moment().format('LL'));
 
-$(".saveBtn").on("click", function(){
+$(".saveBtn").on("click", function () {
   var text = $(this).siblings(".description").val();
-  var time= $(this).parent().attr("id");
+  var time = $(this).parent().attr("id");
 
   localStorage.setItem(time, text);
 })
 
 // load events
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#9 .description").val(localStorage.getItem("hour9"));
+$("#10 .description").val(localStorage.getItem("hour10"));
+$("#11 .description").val(localStorage.getItem("hour11"));
+$("#12 .description").val(localStorage.getItem("hour12"));
+$("#13 .description").val(localStorage.getItem("hour13"));
+$("#14 .description").val(localStorage.getItem("hour14"));
+$("#15 .description").val(localStorage.getItem("hour15"));
+$("#16 .description").val(localStorage.getItem("hour16"));
+$("#17 .description").val(localStorage.getItem("hour17"));
 
-// color coding per time
+// color coding time audit
+var auditTime = function() {
+// get current time
+var currentTime = moment().hour();
+
+// loop over time blocks to audit time (if/else statement)
+$(".time-block").each(function () {
+  
+var eventHour = parseInt($(".time-block").attr("id"));
+
+console.log(eventHour, currentTime)
+
+// if the event hour is less than current time, add .past and remove all others
+if (eventHour < currentTime) {
+  $(this).addClass("past");
+  $(this).removeClass("present");
+  $(this).removeClass("future");
+
+
+// if the eventHour is equal in all ways to the current time, add .present and remove all others
+} else if (eventHour = currentTime) {
+  $(this).addClass("present");
+  $(this).removeClass("past");
+  $(this).removeClass("future");
+
+// if the eventHour is neither of the others, add .future and remove all others
+} else {
+  $(this).addClass("future");
+  $(this).removeClass("present");
+  $(this).removeClass("past");
+}
+});
+}
+// re-run function
+auditTime();
